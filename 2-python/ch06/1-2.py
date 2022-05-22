25810ff18b092224b29b10c0b1c8131edc86de4a
YoungChang Lee <apricotsoul@gmail.com>
2022-05-22 19:19:04 +0900

    feat: deque 활용

---
+++ 2-python/ch06/1-2.py
@@ -0,0 +1,17 @@
+import collections
+from typing import Deque
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+       # 팰린드롬 여부 판별
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
