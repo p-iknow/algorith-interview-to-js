cfe98e81906e90a38e547cff46838097988696c2
YoungChang Lee <apricotsoul@gmail.com>
2022-06-18 21:40:27 +0900

    config: test setting

--- 2_python/ch06/reorder_log_files_test.py
+++ 2_python/ch06/reorder_log_files_test.py
@@ -1,4 +1,7 @@
+import unittest  # The test framework
 from typing import List
+
+# https://leetcode.com/problems/valid-palindrome/
 
 
 def reorder_log_files(logs: List[str]) -> List[str]:
@@ -14,25 +17,30 @@ def reorder_log_files(logs: List[str]) -> List[str]:
     return letters + digits
 
 
-def test_case1():
-    # given
-    logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6",
-            "let2 own kit dig", "let3 art zero"]
-    # when
-    res = reorder_log_files(logs)
+class TestReorderLogFiles(unittest.TestCase):
 
-    # then
-    assert res == [
-        "let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"]
+    def test_case1(self):
+        # given
+        logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6",
+                "let2 own kit dig", "let3 art zero"]
+        # when
+        res = reorder_log_files(logs)
+
+        # then
+        assert res == [
+            "let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"]
+
+    def test_case2(self):
+        # given
+        logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7",
+                "ab1 off key dog", "a8 act zoo"]
+        # when
+        res = reorder_log_files(logs)
+
+        # then
+        assert res == ["g1 act car", "a8 act zoo",
+                       "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"]
 
 
-def test_case2():
-    # given
-    logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7",
-            "ab1 off key dog", "a8 act zoo"]
-    # when
-    res = reorder_log_files(logs)
-
-    # then
-    assert res == ["g1 act car", "a8 act zoo",
-                   "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"]
+if __name__ == '__main__':
+    unittest.main()
