93657249992f6c68e40e0380a055225e8ece22ca
YoungChang Lee <apricotsoul@gmail.com>
2022-06-25 16:56:35 +0900

    config: prettier

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "$schema": "http://json.schemastore.org/prettierrc",
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "bracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 100,
+  "proseWrap": "always",
+  "quoteProps": "as-needed",
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
+  "endOfLine": "auto",
+}
