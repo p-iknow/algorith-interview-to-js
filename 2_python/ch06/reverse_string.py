98b70c94f29c77c279afdfa9a8abecb0ab6e9985
YoungChang Lee <apricotsoul@gmail.com>
2022-06-25 14:00:44 +0900

    apply test

--- 2_python/ch06/reverse_string.py
+++
@@ -1,5 +0,0 @@
-from ast import List
-
-
-def reverse_string(s: List[str]) -> None:
-    s.reverse()
---
+++ 2_python/ch06/reverse_string_test.py
@@ -0,0 +1,32 @@
+import unittest  # The test framework
+from typing import List
+
+# https://leetcode.com/problems/valid-palindrome/
+
+
+def reverse_string(characters: List[str]) -> List[str]:
+    characters.reverse()
+    return characters
+
+
+class TestReverseString(unittest.TestCase):
+
+    def test_case1(self):
+        # given
+        characters = ["h", "e", "l", "l", "o"]
+        # when
+        res = reverse_string(characters)
+        # then
+        self.assertListEqual(res, ["o", "l", "l", "e", "h"])
+
+    def test_case2(self):
+        # given
+        characters = ["H", "a", "n", "n", "a", "h"]
+        # when
+        res = reverse_string(characters)
+        # then
+        self.assertListEqual(res, ["h", "a", "n", "n", "a", "H"])
+
+
+if __name__ == '__main__':
+    unittest.main()
