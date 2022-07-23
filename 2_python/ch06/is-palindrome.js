f2ef045999a687823e14cc3c4ee6e373c06ea3f6
p-iknow <apricotsoul@gmail.comm>
2022-07-23 20:15:40 +0900

    style: lint

--- 2_python/ch06/is-palindrome.js
+++ 2_python/ch06/is-palindrome.js
@@ -2,6 +2,7 @@
  * @param {string} s
  * @return {boolean}
  */
+// eslint-disable-next-line no-unused-vars
 const isPalindrome = s => {
   const regex = /[^A-Za-z0-9]/g; // /[\W_]/g
   const preProcessedStr = s.toLowerCase().replace(regex, '');
