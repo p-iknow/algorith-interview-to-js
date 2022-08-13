3152c7b022d0a9dcdafad1758fc03c470bfbf760
p-iknow <apricotsoul@gmail.com>
2022-08-13 21:47:20 +0900

    feat: two-sum4.test.js

---
+++ 3_linear_data_structures/ch07/two-sum4.test.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const numMap = new Map();
+  for (const [index, number] of nums.entries()) {
+    if (numMap.has(target - number) && numMap.get(target - number) !== index) {
+      return [numMap.get(target - number), index];
+    }
+    numMap.set(number, index);
+  }
+};
+
+describe(`${twoSum.name}`, () => {
+  it('case1', () => {
+    // given
+
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    // when
+    const res = twoSum(nums, target);
+    // then
+    expect(res).toEqual([0, 1]);
+  });
+
+  it('case2', () => {
+    // given
+    const nums = [3, 2, 4];
+    const target = 6;
+    // when
+    const res = twoSum(nums, target);
+    // then
+    expect(res).toEqual([1, 2]);
+  });
+
+  it('case3', () => {
+    // given
+    const nums = [3, 3];
+    const target = 6;
+    // when
+    const res = twoSum(nums, target);
+    // then
+    expect(res).toEqual([0, 1]);
+  });
+});
